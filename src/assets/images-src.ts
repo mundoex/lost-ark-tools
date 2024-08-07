deee5b3c0234d09f93f569f2bc15a6a57b53af18
mundoex <brunoserrao19@gmail.com>
2024-08-07 10:11:30 +0100

    fix image serving

--- src/assets/images-src.ts
+++ src/assets/images-src.ts
@@ -1,4 +1,12 @@
 
+import clearEnergyOfWisdom from "/clear_energy.webp";
+import splendidEnergyOfWisdom from "/splendid_energy.webp";
+import elixir from "/elixir.webp";
+import darkFire from "/dark_fire.webp";
+import magicalSpringWater from "/magical_spring_water.webp";
+import agrisScale from "/agris_scale.webp";
+import alcaoneEye from "/alcaone_eye.webp";
+
 export const GOLD_IMAGE_SRC="https://i.imgur.com/2SWmGw4.png";
 export const SUNSET_SCALE_SRC="https://lostarkcodex.com/icons/use_7_188.webp"
 export const ARGOS_BLOOD_SRC="https://lostarkcodex.com/icons/use_2_202.webp"
@@ -9,10 +17,11 @@ export const PHANTOM_INTENTION_SRC="https://i.imgur.com/z7dMBFs.png";
 export const LIGHTS_TRIAL_SRC="https://i.imgur.com/QencPzO.png";
 export const EMPYREAN_CONTEMPLATION_SRC="https://i.imgur.com/YTX6DKk.png";
 export const EYE_OF_DECAY_SRC="https://i.imgur.com/LrRIH8K.png";
-export const CLEAR_ENERGY_OF_WISDOM_SRC = "lost-ark-tools/clear_energy.webp";
-export const SPLENDID_ENERGY_OF_WISDOM_SRC = "lost-ark-tools/splendid_energy.webp";
-export const ELIXIR_SRC = "lost-ark-tools/elixir.webp";
-export const DARK_FIRE_SRC = "lost-ark-tools/dark_fire.webp";
-export const MAGICAL_SPRING_WATER_SRC="lost-ark-tools/magical_spring_water.webp";
-export const AGRIS_SCALE_SRC="lost-ark-tools/agris_scale.webp";
-export const ALCAONE_EYE_SRC="lost-ark-tools/alcaone_eye.webp";
+
+export const CLEAR_ENERGY_OF_WISDOM_SRC = clearEnergyOfWisdom;
+export const SPLENDID_ENERGY_OF_WISDOM_SRC = splendidEnergyOfWisdom;
+export const ELIXIR_SRC = elixir;
+export const DARK_FIRE_SRC = darkFire;
+export const MAGICAL_SPRING_WATER_SRC = magicalSpringWater;
+export const AGRIS_SCALE_SRC = agrisScale;
+export const ALCAONE_EYE_SRC = alcaoneEye;
